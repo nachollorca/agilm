fc4317c9b444408d3b16c9b114de97a267e4eea0
nachollorca <madwayesp@gmail.com>
2025-01-20 08:49:07 +0100

    refactor import name

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,5 @@
 import pytest
-from agilm.types import Model
+from lamine.types import Model
 
 
 def test_valid_provider_no_locations():
@@ -7,25 +7,30 @@ def test_valid_provider_no_locations():
     assert model.provider == "anthropic"
     assert model.locations is None
 
+
 def test_valid_provider_with_supported_locations():
     model = Model(provider="vertex", id="gemini-1.5-flash-002", locations=["us-central1"])
     assert model.provider == "vertex"
     assert model.locations == ["us-central1"]
+
 
 def test_valid_provider_with_multiple_supported_locations():
     model = Model(provider="vertex", id="gemini-1.5-flash-002", locations=["us-central1", "eu-central1"])
     assert model.provider == "vertex"
     assert model.locations == ["us-central1", "eu-central1"]
 
+
 def test_invalid_location_for_provider():
     with pytest.raises(ValueError) as excinfo:
         Model(provider="anthropic", id="claude-3.5-sonnet-latest", locations=["us-central1"])
     assert str(excinfo.value) == "Provider anthropic does not support `locations`."
 
+
 def test_unsupported_provider():
     with pytest.raises(ValueError) as excinfo:
         Model(provider="unsupported-provider", id="model-id")
     assert str(excinfo.value).startswith("Provider unsupported-provider is not supported:")
+
 
 def test_invalid_location_for_vertex_provider():
     with pytest.raises(ValueError) as excinfo:
@@ -37,6 +42,7 @@ def test_valid_model_id_for_vertex_provider():
     model = Model(provider="vertex", id="gemini-1.5-pro-002")
     assert model.provider == "vertex"
     assert model.id == "gemini-1.5-pro-002"
+
 
 def test_invalid_model_id_for_vertex_provider():
     with pytest.raises(ValueError) as excinfo:
