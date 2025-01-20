394a65674d553371be2031055b6a0ab57b4bbcfc
nachollorca <madwayesp@gmail.com>
2025-01-20 14:04:08 +0100

    Implement batch run and add example in docstring

--- src/lamine/utils.py
+++ src/lamine/utils.py
@@ -4,7 +4,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from typing import Any, Callable
 
 
-def _run_batch(function: Callable, params_list: list[dict[str, Any]], max_workers: int = 10) -> list[Any]:
+def run_in_parallel(function: Callable, params_list: list[dict[str, Any]], max_workers: int = 10) -> list[Any]:
     """
     Executes a batch of calls of a function with different parameters in parallel using threading.
 
@@ -16,6 +16,19 @@ def _run_batch(function: Callable, params_list: list[dict[str, Any]], max_worker
 
     Returns:
         List[Any]: A list containing the results of each function call, in the same order as the input parameters.
+
+    Examples:
+        >>> def add(a, b):
+        ...     return a + b
+
+        >>> params_list = [
+        ...     {"a": 1, "b": 2},
+        ...     {"a": 10, "b": 20},
+        ...     {"a": 100, "b": 200}
+        ... ]
+
+        >>> run_in_parallel(add, params_list)
+        [3, 30, 300]
     """
     results = [None] * len(params_list)
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
