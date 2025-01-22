dba1ac7d09187c0d8e81844a7221129544f7661d
nachollorca <madwayesp@gmail.com>
2025-01-22 13:16:29 +0100

    write utils to parallelize pairs of function-parameters and decorator to return exceptions

--- src/lamine/utils.py
+++ src/lamine/utils.py
@@ -3,11 +3,11 @@
 import logging
 import re
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from pathlib import Path
+from functools import wraps
 from typing import Any, Callable
 
 
-def run_in_parallel(function: Callable, params_list: list[dict[str, Any]], max_workers: int = 10) -> list[Any]:
+def parallelize_function(function: Callable, params_list: list[dict[str, Any]], max_workers: int = 10) -> list[Any]:
     """
     Executes a batch of calls of a function with different parameters in parallel using threading.
 
@@ -18,7 +18,7 @@ def run_in_parallel(function: Callable, params_list: list[dict[str, Any]], max_w
         max_threads (int): The maximum number of threads to use in the thread pool.
 
     Returns:
-        List[Any]: A list containing the results of each function call, in the same order as the input parameters.
+        list[Any]: A list containing the results of each function call, in the same order as the input parameters.
 
     Examples:
         >>> def add(a, b):
@@ -42,7 +42,48 @@ def run_in_parallel(function: Callable, params_list: list[dict[str, Any]], max_w
     return results
 
 
-def parse_xml(tag: str, string: str) -> list[str]:
+def parallelize_functions(
+    function_and_params: list[tuple[Callable, dict[str, Any]]], max_workers: int = 10
+) -> list[Any]:
+    """
+    Executes a batch of different functions in parallel.
+
+    Args:
+        function_and_params (list[tuple[Callable, dict[str, Any]]]): A list of tuples, where each tuple contains
+            a Callable object (function), and a dictionary with the parameters for the function (keys are parameter names).
+        max_workers (int): The maximum number of threads to use in the thread pool.
+
+    Returns:
+        list[Any]: A list containing the results of each function call, in the same order as the input functions and parameters.
+
+    Examples:
+        >>> def add(a, b):
+        ...     return a + b
+
+        >>> def multiply(x, y):
+        ...     return x * y
+
+        >>> function_and_params = [
+        ...     (add, {"a": 1, "b": 2}),
+        ...     (multiply, {"x": 10, "y": 20}),
+        ...     (add, {"a": 100, "b": 200})
+        ... ]
+
+        >>> parallelize_functions(function_and_params)
+        [3, 200, 300]
+    """
+    results = [None] * len(function_and_params)
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures_to_indices = {
+            executor.submit(func, **params): index for index, (func, params) in enumerate(function_and_params)
+        }
+        for future in as_completed(futures_to_indices):
+            index = futures_to_indices[future]
+            results[index] = future.result()
+    return results
+
+
+def parse_xml(tag: str, string: str, should_raise: bool) -> list[str]:
     """
     Parses and extracts content within specified XML tags from a given string.
 
@@ -60,17 +101,32 @@ def parse_xml(tag: str, string: str) -> list[str]:
     )
 
     if len(matches) == 0:
-        details = "f<{tag}> could not be matched"
+        details = f"<{tag}> could not be matched"
         logging.warning(details)
+        if should_raise:
+            raise Exception(details)  # change with ParsingError
         return []
 
     return [match.strip() for match in matches]
 
 
-def is_valid_provider(provider_id: str) -> bool:
-    dir = Path(f"{Path(__file__).parent}/providers")
-    for file in dir.iterdir():
-        if file.is_file() and not file.name.startswith("_") and file.suffix == ".py":
-            if provider_id == file.stem:
-                return True
-    return False
+def return_if_exception(func: Callable) -> Callable:
+    """
+    A decorator that runs a function with given arguments and returns
+    any exception that occurs.
+
+    Args:
+        func (Callable): The function to be decorated.
+
+    Returns:
+        Callable: A wrapped version of the input function.
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any | Exception:
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            return e
+
+    return wrapper
